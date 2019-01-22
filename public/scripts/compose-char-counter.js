2f5413f60d5cea9dac38d3a286442459d4c85fbe
Dylan Leard <dleard@uvic.ca>
2019-01-22 21:54:19 +0000

    implements character counter

---
+++ public/scripts/compose-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("textarea[name='text']").on('input', function constraint(event) {
+    let count = 140 - $(this).val().length;
+    let counterText = $(this).nextAll('.counter')[0];
+    counterText.innerText = count;
+    if (counterText.innerText < 0) { $(counterText).parent().css("color", "red"); }
+    else { $(counterText).parent().css("color", ""); }
+  });
+});
