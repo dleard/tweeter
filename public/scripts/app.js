67b1f4e13e27594960f07075d37328100bfe1553
Dylan Leard <dleard@uvic.ca>
2019-01-23 20:30:16 +0000

    moves ajax post function to app.js, implements reload of tweets on post

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -108,6 +108,31 @@ const loadTweets = () => {
   });
 }
 
+$("form[method='POST'").on('submit', function(event) {
+  event.preventDefault();
+  const data = $(this).serialize();
+  const input = $("textarea[name='text']").val();
+  if (input.length === 0) {
+    alert('Your tweet is empty!');
+    return;
+  }
+  else if (input.length > 140) {
+    alert('Your tweet is too looooong!');
+    return;
+  } else {
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data
+    }).done(function() {
+      $('.tweets').empty();
+      loadTweets();
+      console.log('SUCCESS!');
+      console.log(data);
+    });
+  }  
+});
+
 loadTweets();
 
 });
