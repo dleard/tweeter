add5bd52a341ae6dbd410e804611b6fd1dddd61e
Dylan Leard <dleard@uvic.ca>
2019-01-23 02:14:10 +0000

    removes unnecessary console log

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $("textarea[name='text']").on('input', function constraint(event) {
-    console.log(this);
     let count = 140 - $(this).val().length;
     let counterText = $(this).nextAll('.counter')[0];
     counterText.innerText = count;
